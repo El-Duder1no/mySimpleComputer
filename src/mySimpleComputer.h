06a63e3b349ed2aa41668ef43185fd615e95f8ef
El-Duder1no <doomsday248@yandex.ru>
2021-02-02 13:05:28 +0700

    Add memory array and flag register

--- src/mySimpleComputer.h
+++ src/mySimpleComputer.h
@@ -1,6 +1,14 @@
 #ifndef MY_SIMPLE_COMPUTER_H
 #define MY_SIMPLE_COMPUTER_H
 
+#include <iostream>
+#include <fstream>
+
+#define size 100;
+
+int memory[size];
+int reg;
+
 int sc_memoryInit();
 int sc_memorySet(int address, int value);
 int sc_memoryGet(int address, int* value);
