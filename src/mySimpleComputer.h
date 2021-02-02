740f79d316381d04635973a5f66da5e7b3778180
El-Duder1no <doomsday248@yandex.ru>
2021-02-02 12:20:39 +0700

    Create mySimpleComputer.h

---
+++ src/mySimpleComputer.h
@@ -0,0 +1,15 @@
+#ifndef MY_SIMPLE_COMPUTER_H
+#define MY_SIMPLE_COMPUTER_H
+
+int sc_memoryInit();
+int sc_memorySet(int address, int value);
+int sc_memoryGet(int address, int* value);
+int sc_memorySave(char* filename);
+int sc_memoryLoad(char* filename);
+int sc_regInit();
+int sc_regSet(int register, int value);
+int sc_regGet(int register, int* value);
+int sc_commandEncode(int command, int operand, int* value);
+int sc_commandEncode(int* command, int* operand, int value);
+
+#endif
