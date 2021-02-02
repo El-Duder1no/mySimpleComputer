52f1f66656102b174840c10e15cb458d63e41d6d
El-Duder1no <doomsday248@yandex.ru>
2021-02-02 12:34:05 +0700

    Create mySimpleComputer.cpp

---
+++ src/mySimpleComputer.cpp
@@ -0,0 +1,42 @@
+#include "mySimpleComputer.h"
+
+int sc_memoryInit()
+{
+    return 0;
+}
+int sc_memorySet(int address, int value)
+{
+    return 0;
+}
+int sc_memoryGet(int address, int* value)
+{
+    return 0;
+}
+int sc_memorySave(char* filename)
+{
+    return 0;
+}
+int sc_memoryLoad(char* filename)
+{
+    return 0;
+}
+int sc_regInit()
+{
+    return 0;
+}
+int sc_regSet(int register, int value)
+{
+    return 0;
+}
+int sc_regGet(int register, int* value)
+{
+    return 0;
+}
+int sc_commandEncode(int command, int operand, int* value)
+{
+    return 0;
+}
+int sc_commandEncode(int* command, int* operand, int value)
+{
+    return 0;
+}
