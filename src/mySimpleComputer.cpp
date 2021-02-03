cb3e702ed07667dc6685d82c06a613db901ea9d4
El-Duder1no <doomsday248@yandex.ru>
2021-02-03 18:23:42 +0700

    Minor changes and bugfix

--- src/mySimpleComputer.cpp
+++ src/mySimpleComputer.cpp
@@ -1,5 +1,8 @@
 #include "mySimpleComputer.h"
 
+int memory[size];
+int regFlags;
+
 int sc_memoryInit()
 {
     for(int i = 0; i < size; i++)
@@ -11,49 +14,50 @@ int sc_memoryInit()
 
 int sc_memorySet(int address, int value)
 {
-    if(address <= 0 and address < size)
+    if(address >= 0 and address < size)
     {
         memory[address] = value;
         return 0;
     }
     else
     {
-        std::cout << "OUT_OF_BOUNDS_MEMORY\n"
+        sc_regSet(3, 1);
+        std::cout << "OUT_OF_BOUNDS_MEMORY\n";
         return -1;
     }
 }
-int sc_memoryGet(int address, int* value)
+int sc_memoryGet(int address, int& value)
 {
-    if(address <= 0 and address < size)
+    if(address >= 0 and address < size)
     {
         value = memory[address];
         return 0;
     }
     else
     {
-        std::cout << "OUT_OF_BOUNDS_MEMORY\n"
+        sc_regSet(3, 1);
+        std::cout << "OUT_OF_BOUNDS_MEMORY\n";
         return -1;
     }
 }
 
 int sc_memorySave(char* filename)
 {
-    std::ofstream write(filename, std::ios::binary, std::ios::trunc);
+    std::ofstream write(filename, std::ios::binary | std::ios::trunc);
 
     if(!write.is_open())
     {
         return -1;
     }
-
+        
     for(int i = 0; i < size; i++)
     {
-       write.write((char*)&memory[i], sizeof(memory[i])); 
+        write.write((char*)&memory[i], sizeof(memory[i])); 
     }
 
     write.close();
     return 0;
 }
-
 int sc_memoryLoad(char* filename)
 {
     std::ifstream read(filename, std::ios::binary);
@@ -65,7 +69,7 @@ int sc_memoryLoad(char* filename)
 
     for(int i = 0; i < size; i++)
     {
-       read.read((char*)&memory[i], sizeof(memory[i])); 
+        read.read((char*)&memory[i], sizeof(memory[i])); 
     }
 
     read.close();
@@ -73,25 +77,45 @@ int sc_memoryLoad(char* filename)
 }
 int sc_regInit()
 {
-    reg = 0;
+    regFlags = 0;
     return 0;
 }
 
-int sc_regSet(int register, int value)
+int sc_regSet(int reg, int value)
 {
-    return 0;
+    if(reg >= 1 and reg <= 5)
+    {
+        if(value == 0)
+        {
+            regFlags &= ~(1 << (reg - 1));
+            return 0;
+        }
+        else if(value == 1)
+        {
+            regFlags |= 1 << (reg - 1);
+            return 0;
+        }
+        else 
+            return -1;
+    }
+    else 
+        return -1;
 }
-
-int sc_regGet(int register, int* value)
+int sc_regGet(int reg, int& value)
 {
-    return 0;
+    if(reg >= 1 and reg <= 5)
+    {
+        value = ((regFlags >> (reg - 1)) & 0x1);
+        return 0;
+    }
+    else
+        return -1;
 }
 
 int sc_commandEncode(int command, int operand, int* value)
 {
     return 0;
 }
-
 int sc_commandEncode(int* command, int* operand, int value)
 {
     return 0;
--- src/mySimpleComputer.h
+++ src/mySimpleComputer.h
@@ -4,19 +4,25 @@
 #include <iostream>
 #include <fstream>
 
-#define size 100;
+#define size 100
 
-int memory[size];
-int reg;
+#define OPERATION_OVERFLOW  1
+#define DIVISION_BY_ZERO    2
+#define OUT_OF_BOUNDS       3
+#define CLOCK_PULSE_IGNORE  4
+#define INVALID_COMMAND     5
+
+extern int memory[size];
+extern int regFlags;
 
 int sc_memoryInit();
 int sc_memorySet(int address, int value);
-int sc_memoryGet(int address, int* value);
+int sc_memoryGet(int address, int& value);
 int sc_memorySave(char* filename);
 int sc_memoryLoad(char* filename);
 int sc_regInit();
-int sc_regSet(int register, int value);
-int sc_regGet(int register, int* value);
+int sc_regSet(int reg, int value);
+int sc_regGet(int reg, int& value);
 int sc_commandEncode(int command, int operand, int* value);
 int sc_commandEncode(int* command, int* operand, int value);
 
