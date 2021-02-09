4d9a94232de796c0e96142d685e34fd18ab90600
tryblt <iren09980@icloud.com>
2021-02-09 12:39:56 +0700

    Replace functions with class

--- src/mySimpleComputer.h
+++ src/mySimpleComputer.h
@@ -1,8 +1,36 @@
 #ifndef MY_SIMPLE_COMPUTER_H
 #define MY_SIMPLE_COMPUTER_H
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <array>
+
+class myComputer
+{
+protected:
+    std::array<int, 100> memory;
+    int regFlags;
+    
+public:
+    myComputer();
+    myComputer(const char* filename);
+    
+    int memoryInit();
+    int memorySet(int address, int value);
+    int memoryGet(int address, int &value);
+    int memorySave();
+    int memoryLoad();
+    
+    int regInit();
+    int regSet(int reg, int value);
+    int regGet(int reg, int &value);
+    
+    int commandEncode(int command, int operand, int& value);
+    int commandEncode(int &command, int &operand, int value);
+};
+
+
 
 #define memSize 100
 #define commandsSize 38
@@ -13,6 +41,7 @@
 #define CLOCK_PULSE_IGNORE  4
 #define INVALID_COMMAND     5
 
+/*
 extern int memory[memSize];
 extern int regFlags;
 extern const int commands[];
@@ -27,6 +56,6 @@ int sc_regSet(int reg, int value);
 int sc_regGet(int reg, int& value);
 int cmp(const void *left, const void *right);
 int sc_commandEncode(int command, int operand, int& value);
-int sc_commandDecode(int& command, int& operand, int value);
+int sc_commandDecode(int& command, int& operand, int value);*/
 
 #endif
