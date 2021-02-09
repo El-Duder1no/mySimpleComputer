3b9fc6dfa073b0c3949fe6c50cdffead4741cc6b
El-Duder1no <doomsday248@yandex.ru>
2021-02-09 21:39:31 +0700

    Minor changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,45 @@
 #include "mySimpleComputer.h"
-#include <stdio.h>
+#include <iostream>
+#include <iomanip>
 #include <time.h>
+
+using namespace std;
 
 int main()
 {
     srand(time(nullptr));
 
     int val;
-    char* file = new char[8]{'R', 'A', 'M', '.', 'd', 'a', 't', '\0'};
+    myComputer PC;
 
-    sc_memoryInit();
-    sc_memoryLoad(file);
-
-    for (int i = 0; i < memSize; i++) {
-        sc_memoryGet(i, val);
-        printf("%-5d", val);
-        if ((i + 1) % 10 == 0)
-            printf("\n");
+    PC.memoryLoad("RAM.dat");
+    for(int i = 0; i < PC.memorySize(); i++)
+    {
+        PC.memoryGet(i, val);
+        cout << setw(5) << left << val;
+        if((i + 1) % 10 == 0)
+            cout << "\n";
     }
-    printf("\n");
+    cout << "\n";
 
-    sc_regInit();
-    sc_regSet(OPERATION_OVERFLOW, 0);
-    sc_regSet(DIVISION_BY_ZERO, 1);
-    sc_regSet(OUT_OF_BOUNDS, 1);
-    sc_regSet(CLOCK_PULSE_IGNORE, 0);
-    sc_regSet(INVALID_COMMAND, 0);
+    PC.regSet(myComputer::OPERATION_OVERFLOW,   1);
+    PC.regSet(myComputer::DIVISION_BY_ZERO,     0);
+    PC.regSet(myComputer::OUT_OF_BOUNDS,        1);
+    PC.regSet(myComputer::CLOCK_PULSE_IGNORE,   0);
+    PC.regSet(myComputer::INVALID_COMMAND,      1);
 
-    for (int i = 1; i <= 5; i++) {
-        sc_regGet(i, val);
-        printf("reg %d \n", val);
+    for (int i = 1; i < 6; i++)
+    {
+        PC.regGet(i, val);
+        cout << "Register " << i << " = " << val << "\n";
     }
+    
 
-    int command = 0xA, operand = memory[50];
-    printf("\nCommand - %d\nOperand - %d\n", command, operand);
-    sc_commandEncode(command, operand, val);
-    sc_commandDecode(command, operand, val);
-    printf("\nCommand - %d\nOperand - %d\n", command, operand);
+    int command, operand;
+    cout << "\nCommand = " << command << "\tOperand = " << operand << "\n";
+    PC.commandEncode(0xB, 1000, val);
+    PC.commandDecode(command, operand, val);
+    cout << "Command = " << command << "\tOperand = " << operand << "\n";
 
     return 0;
 }
