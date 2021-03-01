2e159fa32e797588e8af547adeb463f943bcba57
El-Duder1no <doomsday248@yandex.ru>
2021-03-01 18:15:31 +0700

    Add implementation of the myTerm lib

---
+++ src/myTerm.h
@@ -0,0 +1,26 @@
+#ifndef MY_TERM_H
+#define MY_TERM_H
+
+#include <iostream>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+enum Colors
+{
+   BLACK,
+   RED,
+   GREEN,
+   YELLOW,
+   BLUE,
+   PURPLE,
+   LIGHT_BLUE,
+   WHITE,
+};
+
+int clrscr();
+int gotoXY(int row, int col);
+int getScreenSize(int& rows, int& cols);
+int setFGColor(Colors color);
+int setBGColor(Colors color);
+
+#endif
