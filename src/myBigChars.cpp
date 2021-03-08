1db5dfa163e7962f7bd6079b3f85e3212582a483
El-Duder1no <doomsday248@yandex.ru>
2021-03-09 01:15:31 +0700

    Add class methods implementation

--- src/myBigChars.cpp
+++ src/myBigChars.cpp
@@ -1,5 +1,6 @@
 #include "myBigChars.h"
 
+/*
 int printA(const char* str)
 {
     std::cout << "\E(0" << str << "\E(B";
@@ -42,7 +43,7 @@ int printBox(int x, int y, int height, int width)
     return 0;
 }
 
-int printBigChar(int arr[2], int x, int y, Colors fgColor, Colors bgColor)
+int printBigChar(long int arr[2], int x, int y, Colors fgColor, Colors bgColor)
 {
     int row, col;
     getScreenSize(row, col);
@@ -90,5 +91,158 @@ int bigCharWrite(int fd, int& big, int need_count, int count)
 
 int bigCharRead(int fd, int& big, int need_count, int& count)
 {
+    return 0;
+}
+*/
+
+myBigChar::myBigChar(myBigChar::BigChar symbol)
+{
+    switch(symbol)
+    {
+    case BigChar::Zero:
+        this->symbol = char_collection[0];
+        break;
+    case BigChar::One:
+        this->symbol = char_collection[1];
+        break;
+    case BigChar::Two:
+        this->symbol = char_collection[2];
+        break;
+    case BigChar::Three:
+        this->symbol = char_collection[3];
+        break;
+    case BigChar::Four:
+        this->symbol = char_collection[4];
+        break;
+    case BigChar::Five:
+        this->symbol = char_collection[5];
+        break;
+    case BigChar::Six:
+        this->symbol = char_collection[6];
+        break;
+    case BigChar::Seven:
+        this->symbol = char_collection[7];
+        break;
+    case BigChar::Eight:
+        this->symbol = char_collection[8];
+        break;  
+    case BigChar::Nine:
+        this->symbol = char_collection[9];
+        break;
+    case BigChar::Plus:
+        this->symbol = char_collection[10];
+        break;
+    case BigChar::Minus:
+        this->symbol = char_collection[11];
+        break; 
+    }
+}
+
+int myBigChar::printA(const char* str)
+{
+    std::cout << "\E(0" << str << "\E(B";
+    return 0;
+}
+
+int myBigChar::printBox(int x, int y, int height, int width)
+{
+    int row, col;
+    getScreenSize(row, col);
+    if((x <= 0  or x >= row) or (y <= 0 or y >= col))
+        return -1;
+
+    gotoXY(x + height, y);
+    printA(BOT_LEFT_CORNER);
+    gotoXY(x, y);
+    printA(TOP_LEFT_CORNER);
+    gotoXY(x + height, y + width);
+    printA(BOT_RIGHT_CORNER);
+    gotoXY(x, y + width);
+    printA(TOP_RIGHT_CORNER);
+
+    for(int i = x + 1; i < x + height; i++)
+    {
+        gotoXY(i, y);
+        printA(VERTICAL_LINE);
+        gotoXY(i, y + width);
+        printA(VERTICAL_LINE);
+    }
+
+    for(int i = y + 1; i < y + width; i++)
+    {
+        gotoXY(x, i);
+        printA(HORIZONTAL_LINE);
+        gotoXY(x + height, i);
+        printA(HORIZONTAL_LINE);
+    }
+    std::cout << "\n";
+    
+    return 0;
+}
+
+int myBigChar::print(int x, int y, Colors fgColor, Colors bgColor)
+{
+    int row, col;
+    getScreenSize(row, col);
+    if((x <= 0  or x >= row) or (y <= 0 or y >= col))
+        return -1;
+
+    gotoXY(x, y);
+    setFGColor(fgColor);
+    setBGColor(bgColor);
+
+    char string[8] {};
+    for(int i = 0; i < 2; i++)
+    {
+        for(int j = 0; j < 4; j++)
+        {
+            int temp = symbol[i] >> (j * 8);
+            for(int k = 0; k < 8; k++)
+            {
+                int bit = (temp & (1 << k));
+                bit != 0 ? string[k] = BLOCK : string[k] = ' ';
+            }
+            gotoXY(x + (i * 4) + j + 1, y);
+            printA(string);
+        }
+    }
+    setDefaultColor();
+    gotoXY(x + 10, 0);
+    return 0;
+}
+
+int myBigChar::setPos(int* big, int x, int y, int value)
+{
+    if(x < 0 or x > 7 or y < 0 or y > 7)
+        return -1;
+    if(value == 1)
+        big[x / 4] |= 1 << ((x % 4) * 8 + y);
+    else if(value == 0)
+        big[x / 4] &= ~(1 << ((x % 4) * 8 + y));
+    
+    return 0;
+}
+
+int myBigChar::getPos(int* big, int x, int y, int& value)
+{
+    if(x < 0 or x > 7 or y < 0 or y > 7)
+        return -1;
+
+    value = (big[x / 4] >> ((x % 4) * 8 + y)) & 0x1;
+    
+    return 0;
+}
+
+int myBigChar::writeBC(int fd, int* big, int need_count, int count)
+{
+    ::write(fd, big, count * sizeof(int) * 2);
+    close(fd);
+    return 0;
+}
+
+int myBigChar::readBC(int fd, int* big, int need_count, int& count)
+{
+    read(fd, big, need_count * sizeof(int) * 2);
+    close(fd);
     return 0;
 }
--- src/myBigChars.h
+++ src/myBigChars.h
@@ -2,20 +2,66 @@
 #define MY_BIGCHARS_H
 
 #include "myTerm.h"
+#include <array>
+#include <unistd.h>
 
-#define TOP_LEFT_CORNER     "l"
-#define TOP_RIGHT_CORNER    "k"
-#define BOT_LEFT_CORNER     "m"
-#define BOT_RIGHT_CORNER    "j"
-#define HORIZONTAL_LINE     "q"
-#define VERTICAL_LINE       "x"
- 
-int printA(const char* str);
-int printBox(int x, int y, int height, int width);
-int printBigChar(int arr[2], int x, int y, Colors fgColor, Colors bgColor);
-int setBigCharPos(int* big, int x, int y, int value);
-int getBigCharPos(int& big, int x, int y, int& value);
-int bigCharWrite(int fd, int& big, int need_count, int count);
-int bigCharRead(int fd, int& big, int need_count, int& count);
+#define TOP_LEFT_CORNER      "l"
+#define TOP_RIGHT_CORNER     "k"
+#define BOT_LEFT_CORNER      "m"
+#define BOT_RIGHT_CORNER     "j"
+#define HORIZONTAL_LINE      "q"
+#define VERTICAL_LINE        "x"
+
+#define BLOCK                'a'
+
+typedef std::array<unsigned, 2> BC_ARRAY;
+
+class myBigChar
+{
+protected: 
+    const std::array<BC_ARRAY, 12> char_collection
+    {
+        3'890'741'118,  2'130'700'263,  // 0
+        943'602'744,    943'208'504,    // 1
+        1'894'252'414,  4'294'903'580,  // 2
+        2'145'451'903,  2'147'475'680,  // 3
+        4'286'019'447,  1'886'417'151,  // 4
+        2'131'230'719,  2'130'698'495,  // 5
+        2'131'230'590,  2'130'700'287,  // 6
+        1'893'793'791,  471'604'280,    // 7
+        2'129'133'438,  2'129'127'399,  // 8
+        4'293'394'302,  2'130'698'494,  // 9
+        2'115'508'224,  1'579'134,      // +
+        2'113'929'216,  126             // -
+    }; 
+    BC_ARRAY symbol;
+
+public:
+    enum BigChar
+    {
+        Zero, One,  Two,    Three,  Four,
+        Five, Six,  Seven,  Eight,  Nine,
+        Plus, Minus 
+    };
+
+    myBigChar(BigChar symbol);
+
+    int printA(const char* str);
+    int printBox(int x, int y, int height, int width);
+    int print(int x, int y, Colors fgColor, Colors bgColor);
+    int setPos(int* big, int x, int y, int value);
+    int getPos(int* big, int x, int y, int& value);
+    int writeBC(int fd, int* big, int need_count, int count);
+    int readBC(int fd, int* big, int need_count, int& count);
+
+};
+
+// int printA(const char* str);
+// int printBox(int x, int y, int height, int width);
+// int printBigChar(long int arr[2], int x, int y, Colors fgColor, Colors bgColor);
+// int setBigCharPos(int* big, int x, int y, int value);
+// int getBigCharPos(int& big, int x, int y, int& value);
+// int bigCharWrite(int fd, int& big, int need_count, int count);
+// int bigCharRead(int fd, int& big, int need_count, int& count);
 
 #endif
